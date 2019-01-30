c6af7eecadf1e7b0ec4121b330961dd38d3f6444
Joe Mumm <jmummdbd@att.net>
2019-01-30 11:10:20 -0800

    Initial commit.  Created Flogger.Core project.

---
+++ Flogging.Core/Flogger.cs
@@ -0,0 +1,44 @@
+﻿using Serilog;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flogging.Core
+{
+  public static class Flogger
+  {
+    private static readonly ILogger _perfLogger;
+    private static readonly ILogger _useageLogger;
+    private static readonly ILogger _errorLogger;
+    private static readonly ILogger _diagnosticsLogger;
+
+    static Flogger()
+    {
+      var path = @"D:\DevProj\Trng\NetLgngSeriDoneRight\Logs\";
+      _perfLogger = new LoggerConfiguration()
+        .WriteTo.File(path: path + "perf.txt").CreateLogger();
+      _useageLogger = new LoggerConfiguration()
+        .WriteTo.File(path: path + "usage.txt").CreateLogger();
+      _errorLogger = new LoggerConfiguration()
+        .WriteTo.File(path: path + "error.txt").CreateLogger();
+      _diagnosticsLogger = new LoggerConfiguration()
+        .WriteTo.File(path: path + "diagnostic.txt").CreateLogger();
+    }
+
+    public static void WritePerf(FlogDetail infoToLog)
+    { _perfLogger.Write(LogEventLevel.Information, "{@FlogDetail}", infoToLog); }
+    public static void WriteUseage(FlogDetail infoToLog)
+    { _useageLogger.Write(LogEventLevel.Information, "{@FlogDetail}", infoToLog); }
+    public static void WriteError(FlogDetail infoToLog)
+    { _errorLogger.Write(LogEventLevel.Information, "{@FlogDetail}", infoToLog); }
+    public static void WriteDiagnostic(FlogDetail infoToLog)
+    {
+      var writeDiagnostics = Convert.ToBoolean(ConfigurationManager.AppSettings["EnableDiagnostics"]);
+      if (!writeDiagnostics) return;
+      _diagnosticsLogger.Write(LogEventLevel.Information, "{@FlogDetail}", infoToLog); }
+  }
+}
---
+++ Flogging.Core/PerfTracker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flogging.Core
+{
+  public class PerfTracker
+  {
+    private readonly Stopwatch _sw;
+    private readonly FlogDetail _infoToLog;
+
+    public PerfTracker(string name, string userId, string userName,
+        string location, string product, string layer) {
+      _sw = Stopwatch.StartNew();
+      _infoToLog = new FlogDetail() {
+        Message = name, UserId = userId, UserName = userName,
+        Product = product, Layer = layer, Location = location,
+        Hostname = Environment.MachineName };
+
+      var beginTime = DateTime.Now;
+      _infoToLog.AdditionalInfo = new Dictionary<string, object>()
+      {
+        { "Started", beginTime.ToString(CultureInfo.InvariantCulture) }
+      };
+    }
+
+    public PerfTracker(string name, string userId, string userName,
+          string location, string product, string layer,
+          Dictionary<string, object> perfParams)
+        : this(name, userId, userName, location, product, layer) {
+      foreach (var item in perfParams)
+      { _infoToLog.AdditionalInfo.Add("input-" + item.Key, item.Value); }
+    }
+
+    public void Stop() {
+      _sw.Stop();
+      _infoToLog.ElapsedMilliseconds = _sw.ElapsedMilliseconds;
+      Flogger.WritePerf(_infoToLog);
+    }
+
+  }
+}
